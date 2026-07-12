f4339fada2c8aacf64a66aa2ab4a86e28611e25d
TEAM-WAD <92700528+TEAM-WAD@users.noreply.github.com>
2026-07-12 06:36:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 from pyrogram import Client, filters
-from tgcalls import GroupCallFactory
+from tgcalls import GroupCall
 
 app = Client("MyBot", api_id=30357243, api_hash="9782c26101c502f026721b8e7993786c", bot_token="8892595660:AAFIQO5THzLYYP1wvs56w_Ln1V8cjpFkDlo")
-group_call_factory = GroupCallFactory(app)
+group_call = GroupCall(app)
 
 @app.on_message(filters.command("play"))
 async def play(client, message):
-    group_call = group_call_factory.get_group_call(message.chat.id)
     await group_call.join(message.chat.id)
     await group_call.start_audio("https://download.samplelib.com/mp3/sample.mp3")
-    await message.reply("تم التشغيل بواسطة TgCalls!")
+    await message.reply("تم التشغيل بنجاح!")
 
 app.run()
